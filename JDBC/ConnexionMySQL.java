b831bfdf7fb18971bc934360c4e6627319379e0f
Damien Martin <damien.martin1@etu.univ-orleans.fr>
2015-06-10 14:30:44 +0200

    modificationn de ConnexionMySQL pour permettre de se connecter sur sa base de données en rentrant son mot de passe

--- JDBC/ConnexionMySQL.java
+++ JDBC/ConnexionMySQL.java
@@ -1,69 +1,72 @@
 import java.sql.*;
+import java.util.Scanner;
 
 /**
  * @author Damien MARTIN
  */
 
-public class ConnexionMySQL{
-    // attributs
-    public Connection mysql;
-    boolean ouverte;
+public class ConnexionMySQL {
+	// attributs
+	public Connection mysql;
+	boolean ouverte;
+	Scanner sc = null;
+	String mdp;
 
-    // constructeur
-    public ConnexionMySQL(String nom_serveur, String nom_base, String login, String repertoire_principal){
-	try{
-	    Class.forName("com.mysql.jdbc.Driver");
+	// constructeur
+	public ConnexionMySQL(String nom_serveur, String nom_base, String login, String repertoire_principal) {
+		sc = new Scanner(System.in);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception ex) {
+			System.out.println("classe non trouvée");
+		}
+		try {
+			System.out.print("Entrez votre mot de passe de MySQL"+' ');
+			mdp= sc.nextLine();
+			mysql = DriverManager.getConnection(nom_serveur + nom_base, login, mdp);
+			ouverte = true;
+		} catch (SQLException ex) {
+			System.out.println("Msg: " + ex.getMessage() + ex.getErrorCode());
+		}
 	}
-	catch (Exception ex){
-	    System.out.println("classe non trouvée");
-	}
-	try{
-	    mysql = DriverManager.getConnection(nom_serveur+nom_base, login, "dmartin");
-	    ouverte=true;
-	}
-	catch (SQLException ex){
-	    System.out.println("Msg: "+ex.getMessage()+ex.getErrorCode());
-	}
-    }
 
-    public void reconnexion(String nom_serveur, String nom_base, String login, String repertoire_principal){
-	try{
-	    mysql = DriverManager.getConnection(nom_serveur, login, "dmartin");
+	public void reconnexion(String nom_serveur, String nom_base, String login, String repertoire_principal) {
+		try {
+			mysql = DriverManager.getConnection(nom_serveur, login, mdp);
+		} catch (SQLException ex) {
+			System.out.println("Msg:" + ex.getMessage() + ex.getErrorCode());
+		}
 	}
-	catch (SQLException ex){
-	    System.out.println("Msg:"+ex.getMessage()+ex.getErrorCode());
-	}
-    }
-    
-    public boolean estConnecte(){
-	return ouverte;
-    }
-	
-    public void deconnexion(){
-	try{
-	    mysql.close();
-	    ouverte=false;
-	    System.out.println("déconnexion réussie");
-	}
-	catch(SQLException e){
-	    System.out.println("pb de déconnexion");
-	}
-    }
 
-    public void reconnexion(String loginMySQL, String mdp){
-	try{
-	    mysql = DriverManager.getConnection("jdbc:mysql://servinfo-db:3306/"+loginMySQL,loginMySQL, "dmartin");
+	public boolean estConnecte() {
+		return ouverte;
 	}
-	catch(SQLException e){
-	    System.out.println("Connection Failed");
-	    e.printStackTrace();
-	    return;
-	}
-	ouverte = true;
-	System.out.println("connexion réussie");
-    }
 
-    public Connection getConnexion(){
-	return mysql;
-    }
+	public void deconnexion() {
+		try {
+			mysql.close();
+			ouverte = false;
+			System.out.println("déconnexion réussie");
+		} catch (SQLException e) {
+			System.out.println("pb de déconnexion");
+		}
+	}
+
+	public void reconnexion(String loginMySQL, String mdp) {
+		try {
+			mysql = DriverManager.getConnection(
+					"jdbc:mysql://servinfo-db:3306/" + loginMySQL, loginMySQL,
+					"dmartin");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed");
+			e.printStackTrace();
+			return;
+		}
+		ouverte = true;
+		System.out.println("connexion réussie");
+	}
+
+	public Connection getConnexion() {
+		return mysql;
+	}
 }

d7f750bc73a7d88da80959f60584466c3c9274a5
Benjamin.Repulles <benjamin.repulles@etu.univ-orleans.fr>
2015-06-10 11:19:00 +0200

    ajout de la partie client  sur la partie reponses aux question des modifications restent a faire en fonction de la base de donnee

---
+++ Benjamin/Fenetre.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+
+
+import javax.swing.*;
+
+
+public class Fenetre extends JFrame{
+	static Module m;
+	Fenetre(){
+		
+		super("Rapid'Sond");
+		System.out.println("Fenetre deb");
+
+		this.setSize(1000,800);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		
+		Container c = this.getContentPane();
+
+			// Menu
+		this.setJMenuBar(new Menu());
+		System.out.println("ajout menu");
+		
+			// creation espace module
+		 m = new ModuleSondage();
+
+		c.add(m);
+		
+				// affichage en pleine ecran
+		GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		if (device.isFullScreenSupported()) {
+			device.setFullScreenWindow(this);
+	    } else {
+	        System.err.println("Le mode plein ecran n'est pas disponible");
+	    }
+	    
+
+		
+
+	    
+	    
+		 this.setVisible(true);
+	}
+	
+	
+	
+	@SuppressWarnings("unused")
+	public static void main ( String [] args){
+		new Fenetre();
+		
+	}
+
+}

053d6c18f3c1d5d09082a6dfb6d3cacf8312fe5d
anais <anais.lavorel@etu.univ-orleans.fr>
2015-06-10 01:31:08 -0700

    modification du Module SOndage et supression des version test inutil

--- Anais/QuestionnaireVue.java
+++ Anais/QuestionnaireVue.java
@@ -4,16 +4,29 @@ import java.awt.*;
 public class QuestionnaireVue extends JPanel{
 	//static Questionnaire q, Sonde s;
 	private JLabel idQuestionnaire;
+	private QuestionVue q;
+	
 	// pour l'instant je crée un affichage vide, mais par la suite le constructeur prendra en parametre:
 	//		Questionnaire q, Sonde s
 	QuestionnaireVue(){
 		super();
-		this.setLayout(new BorderLayout());
+		this.setLayout(new GridLayout(2,1));
+
 		
 		// Creation du label Questionnaire:
 		
+		JPanel idQuestionnaireJ = new JPanel(new FlowLayout());
 		idQuestionnaire = new JLabel("Questionnaire n° X");
-		this.add(idQuestionnaire, "Center");
+		idQuestionnaireJ.add(idQuestionnaire);
+		
+		// réation de la vue de la question:
+			// par la suite on donnera une question et il creera la vue en fonction de la question
+		q = new QuestionVue();
+		
+		
+		//this.add(new JLabel("TESTTTTT"));
+		this.add(idQuestionnaireJ);
+		this.add(q);
 		
 		
 		

c80aa5f704048be5886d234e60625f6720c3df05
Damien Martin <damien.martin1@etu.univ-orleans.fr>
2015-06-10 11:28:59 +0200

    ajout de SondeBD et de Sonde permettant de récupérer la liste des sondés et ajout de .gitignore pour ignorer les .class

---
+++ JDBC/SondeBD.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author dmartin
+ * 
+ */
+public class SondeBD {
+	private ConnexionMySQL c;
+	private Statement s;
+
+	// constructeur
+	public SondeBD(ConnexionMySQL c) throws SQLException {
+		// on crée un nouveau UtilisateurBD
+		try {
+			this.c = c;
+			Connection conn = c.getConnexion();
+			this.s = conn.createStatement();
+			System.out.println("SondeBD créé");
+		} catch (SQLException e) {
+			System.out.println("SondeBD non créé");
+		}
+	}
+
+	public ArrayList<Sonde> getListeSonde() {
+		ArrayList<Sonde> res = new ArrayList<Sonde>();
+		try {
+			ResultSet rs = s.executeQuery("SELECT * FROM SONDE");
+			while (rs.next()) {
+				res.add(new Sonde(rs.getInt("numSond"), rs.getString("nomSond"),
+						rs.getString("prenomSond"), rs.getInt("dateNaisSond"),
+						rs.getString("telephoneSond"), rs.getString("idC")));
+			}
+
+		} 
+		catch (SQLException e) {System.out.println(e);}
+		return res;
+	}
+}

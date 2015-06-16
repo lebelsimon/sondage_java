5bb24081510aa490c86e11a078f535aae00cfd5f
Benjamin.Repulles <benjamin.repulles@etu.univ-orleans.fr>
2015-06-16 10:02:48 +0200

    ajout du dossier commun plus positionnement des IHM dans la fenetre

---
+++ CommunAB/Fenetre.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Fenetre extends JFrame{
+	static Module m;
+	Fenetre(){
+		super("Rapid'Sond");
+		System.out.println("Fenetre deb");
+
+		this.setSize(800,600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		
+		Container c = this.getContentPane();
+
+			// Menu
+		this.setJMenuBar(new Menu());
+		System.out.println("ajout menu");
+		
+			// creation espace module
+		m = new ModuleSondage();
+		
+		c.add(m,"Center");
+		
+				// affichage en pleine ecran
+		//~ GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		//~ if (device.isFullScreenSupported()) {
+			//~ device.setFullScreenWindow(this);
+	    //~ } else {
+	        //~ System.err.println("Le mode plein ecran n'est pas disponible");
+	    //~ }
+	    
+
+		
+
+	    
+	    
+		 this.setVisible(true);
+	}
+	
+	
+	
+	@SuppressWarnings("unused")
+	public static void main ( String [] args){
+		new Fenetre();
+		
+	}
+
+}
---
+++ CommunAB/Sonde.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+
+/**
+ * @author dmartin
+ *
+ */ 
+public class Sonde {
+	private int numSond, dateNaissSond;
+	private String nomSond, prenomSond, telephoneSond;
+	private String idC;
+	
+	// constructeur
+	public Sonde(int numSond, String nomSond, String prenomSond, int dateNaissSond, String telephone, String idCategorie){
+		this.numSond=numSond;
+		this.nomSond=nomSond;
+		this.prenomSond=prenomSond;
+		this.dateNaissSond=dateNaissSond;
+		this.telephoneSond=telephone;
+		this.idC=idCategorie;
+	}
+	
+	public Sonde(Sonde s){
+		this.numSond=s.getNumSond();
+		this.nomSond=s.getNomSond();
+		this.prenomSond=s.getPrenomSond();
+		this.dateNaissSond=s.getDateNaissSond();
+		this.telephoneSond=s.getTelephoneSond();
+		this.idC=s.getIdC();
+}
+	
+	// getters et setters
+	public int getNumSond() {
+		return numSond;
+	}
+	public void setNumSond(int numSond) {
+		this.numSond = numSond;
+	}
+	public int getDateNaissSond() {
+		return dateNaissSond;
+	}
+	public void setDateNaissSond(int dateNaissSond) {
+		this.dateNaissSond = dateNaissSond;
+	}
+	public String getNomSond() {
+		return nomSond;
+	}
+	public void setNomSond(String nomSond) {
+		this.nomSond = nomSond;
+	}
+	public String getPrenomSond() {
+		return prenomSond;
+	}
+	public void setPrenomSond(String prenomSond) {
+		this.prenomSond = prenomSond;
+	}
+	public String getTelephoneSond() {
+		return telephoneSond;
+	}
+	public void setTelephoneSond(String telephoneSond) {
+		this.telephoneSond = telephoneSond;
+	}
+	public String getIdC() {
+		return idC;
+	}
+	public void setIdC(String idC) {
+		this.idC = idC;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Sonde [numSond=" + numSond + ", dateNaissSond=" + dateNaissSond
+				+ ", nomSond=" + nomSond + ", prenomSond=" + prenomSond
+				+ ", telephoneSond=" + telephoneSond + ", idC=" + idC + "]";
+	}
+	
+	
+
+}

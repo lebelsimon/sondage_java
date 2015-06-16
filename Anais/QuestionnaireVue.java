ba4ebf02e85b4a5168c3d4e4b7859bb55ca2da75
anais.lavorel <anais.lavorel@etu.univ-orleans.fr>
2015-06-16 08:39:10 +0200

    mise en place du focusChamp et du creation de la class ActionButton pour le questionnaire

--- Anais/QuestionnaireVue.java
+++ Anais/QuestionnaireVue.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 
 public class QuestionnaireVue extends JPanel{
 	//static Questionnaire q, Sonde s;
 	private JLabel idQuestionnaire;
-	private QuestionVue q;
+	private Questionnaire q;
+	
 	
 	// pour l'instant je crée un affichage vide, mais par la suite le constructeur prendra en parametre:
 	//		Questionnaire q, Sonde s
@@ -20,30 +23,44 @@ public class QuestionnaireVue extends JPanel{
 		idQuestionnaireJ.add(idQuestionnaire);
 		idQuestionnaireJ.setBorder(BorderFactory.createLineBorder(Color.black));
 		
+		
 		// creation des boutons et de leur panel
 		
 		JPanel boutonsQuestions = new JPanel();
+		
 		boutonsQuestions.setBorder(BorderFactory.createLineBorder(Color.black));
 		JButton qsuiv = new JButton(">");
+		qsuiv.setName("suiv");
+		qsuiv.addActionListener(new QuestionnaireButton(qsuiv.getName(), this.q));
+		
 		JButton qprec = new JButton("<");
-		JButton qprem = new JButton("<<");
+		qprec.setName("prec");
+		qprec.addActionListener(new QuestionnaireButton(qprec.getName(), this.q));
+		
+		JButton qprem = new JButton("<<<");
+		qprem.setName("prem");
+		qprem.addActionListener(new QuestionnaireButton(qprem.getName(), this.q));
+		
 		JButton qder = new JButton(">>>");
+		qder.setName("dern");
+		qder.addActionListener(new QuestionnaireButton(qder.getName(), this.q));
 		
 		boutonsQuestions.add(qprem); 
 		boutonsQuestions.add(qprec);
 		boutonsQuestions.add(qsuiv); 
 		boutonsQuestions.add(qder); 
 
-		// réation de la vue de la question:
+		// creation de la vue de la question:
 			// par la suite on donnera une question et il creera la vue en fonction de la question
-		q = new QuestionVueChoixOrdonnee();
+		QuestionVue questionVue = new QuestionVueChoixOrdonnee();
 		
-		q.setBorder(BorderFactory.createLineBorder(Color.black));
-
+		questionVue.setBorder(BorderFactory.createLineBorder(Color.black));
+		questionVue.setPreferredSize(new Dimension(300,150));
+		questionVue.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
 		//this.add(new JLabel("TESTTTTT"));
 		this.add(idQuestionnaireJ, BorderLayout.NORTH);
-		this.add(q, BorderLayout.CENTER);
+		this.add(questionVue, BorderLayout.CENTER);
 		this.add(boutonsQuestions,BorderLayout.SOUTH); 		
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		

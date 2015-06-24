459f954f6ebb62c909008ff53fb2287fb0af928f
anais.lavorel <anais.lavorel@etu.univ-orleans.fr>
2015-06-24 09:26:22 +0200

    test commit pour acces bd reponce

--- Anais/QuestionVue.java
+++ Anais/QuestionVue.java
@@ -1,40 +1,89 @@
 import javax.swing.*;
+
 import java.awt.*;
+import java.util.ArrayList;
 
 public class QuestionVue extends JPanel{
-	private JLabel idQuestion;
-	private JTextField enonce;
-	static Question question;
-	static boolean estRemplie;
+	JLabel idQuestion;
+	JTextArea enonce;
+	int numQuestion;
+	int nombreVal;
+	char typeQuestion ;
+	DefaultListModel<Proposition> propositions;
+	Question question;
+	QuestionnaireVue questionnaireVue;
+	String enonceQuestion;
+	JButton suprSelect;
+	JPanel choix ;
 	
-	private DefaultListModel<Proposition> listeProp;
-	
-	QuestionVue(){
+	QuestionVue( Question q, QuestionnaireVue questV){
+		this.setLayout( new BorderLayout( 40,50));
+		question = q;
+		questionnaireVue = questV;
+		// initialisation des valeurs corespondant au elements de la Question
+		numQuestion = question.getNumQ();
+		propositions = question.getPropositions();
+		nombreVal = propositions.size();
+		typeQuestion = question.getIdT();
+		enonceQuestion = question.getTexteQuestion();
 		
-		// init 
-		estRemplie = false;
-		String numQuestion = "Question n° "+ question.numQ;
-		String enonceQuestion = question.getTexteQuestion();
-		DefaultListModel<Proposition>  listeProp = question.propositions;
 		
-		this.setLayout( new BorderLayout( 40, 4));
 		
-		idQuestion = new JLabel(numQuestion);
+		// creations des elements de la vue
+		idQuestion = new JLabel("Question numero: "+numQuestion + " / "+ questionnaireVue.module.questionnaire.getListeQuestions().size());
 		idQuestion.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		enonce = new JTextField(enonceQuestion);
+		enonce = new JTextArea(enonceQuestion);
 		enonce.setEditable(false);
+		enonce.setLineWrap(true);
 		
-		
-		JPanel reponces = new JPanel();
-		
-		//reponces.add(new JLabel("LES REPONSES"));
-		
+		//creation du panel contenant l'identifiant et l'enonce de la question	
 		JPanel haut = new JPanel(new GridLayout(2,1));
+		
+			// suprimer la selection
+		suprSelect = new JButton("Supprimer la Selection");
+		suprSelect.setName("supr");
+		suprSelect.setToolTipText("Supprimer la selection, remettre la reponce a '' ");
+		suprSelect.addActionListener(new QuestionButton(this));
+		suprSelect.setAlignmentX(CENTER_ALIGNMENT);
+
+		this.add(suprSelect, "South");
+		
+		// ajout des element a la vue QUestion
 		haut.add(idQuestion);
 		haut.add(enonce);
 		this.add(haut,"North");
+		
+		// creation des proposition
+		choix = this.creerProposition(typeQuestion);
+		this.add(choix, "Center");
+		this.revalidate();
+		this.repaint();
 	}
+	
+	// cree la liste des propositon en fonction du type de question
+	public JPanel creerProposition(char typeQuestion){
+		JPanel res; 
+		if (typeQuestion=='l'){
+			res = new PropositionChoixLibre(this);
+		}
+		else if( typeQuestion=='c'){
+			res = new PropositionChoixOrdonnee(this);
+		}
+		else if ( typeQuestion=='m'){
+			res = new PropositionChoixX(this);
+		}
+		else if( typeQuestion=='n'){
+			res = new PropositionChoixNote(this);
+		}
+		else{
+			res = new PropositionChoix1(this);
+		}
+		return res;
+	}
+	
+	
+		
 }
 		
 		
--- Anais/QuestionnaireVue.java
+++ Anais/QuestionnaireVue.java
@@ -1,73 +1,159 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class QuestionnaireVue extends JPanel{
 	//static Questionnaire q, Sonde s;
-	private JLabel idQuestionnaire;
+	
+	JLabel titreQuestionnaire;
 	Questionnaire questionnaire;
+	ModuleSondage module;
+	QuestionVue questionVue;
+	QuestionnaireNonRempli qError;
+	//Question question;
+	int largeur, hauteur;
+	JPanel boutonsQuestions;
+	JButton qprec, qsuiv, qprem, qder, terminer;
+	JPanel idQuestionnaireJ, questionJ;
 	
 	
-	// pour l'instant je crée un affichage vide, mais par la suite le constructeur prendra en parametre:
-	//		Questionnaire q, Sonde s
-	QuestionnaireVue(){
+QuestionnaireVue(ModuleSondage mod){
+		// initialisation des valeurs
 		super();
-		this.setLayout(new BorderLayout());
+		System.out.println("DEB crea Vue questionnaire");
 
-		// initialisation
-		String nomQuestionnaire = questionnaire.getTitreQuestionnaire();
+		this.module = mod;
+		this.questionnaire = module.questionnaire;// BD.getListeQuestionnaire().get(numQuestionnaire-1);
+		//this.question = questionnaire.getListeQuestions().get(0);
+		System.out.println(module.fenetre);
+		largeur = module.fenetre.getWidth()/2 - module.fenetre.getWidth()/10;
+		hauteur = module.fenetre.getHeight() - module.fenetre.getHeight()/10;
+		this.setPreferredSize(new Dimension(largeur, hauteur));
 		
+		this.setLayout(new BorderLayout());
+		Random random = new Random();
+		//System.out.println(module.sonde);//.getNumSond());
+		int numSonde = module.sonde.getNumSond();//199;
+		questionVue = new QuestionVue(module.questionnaire.getListeQuestions().get(0), this);
+
+		int numQuestionnaire = module.questionnaire.idQ; 
+		System.out.println("Indice calculer");
+
+		
+
 		// Creation du label Questionnaire:
 		
-		JPanel idQuestionnaireJ = new JPanel(new FlowLayout(FlowLayout.CENTER, 40, 4));
-		
-		
-		idQuestionnaire = new JLabel(nomQuestionnaire);
-		idQuestionnaireJ.add(idQuestionnaire);
+		idQuestionnaireJ = new JPanel(new FlowLayout(FlowLayout.CENTER, 40, 4));
+		titreQuestionnaire = new JLabel(module.questionnaire.getTitreQuestionnaire());
+		idQuestionnaireJ.add(titreQuestionnaire);
 		idQuestionnaireJ.setBorder(BorderFactory.createLineBorder(Color.black));
 		
-		
+		System.out.println("CREATION BOUTONS");
 		// creation des boutons et de leur panel
+		boutonsQuestions = new JPanel();
 		
-		JPanel boutonsQuestions = new JPanel();
-		
+			// question suivante
 		boutonsQuestions.setBorder(BorderFactory.createLineBorder(Color.black));
-		JButton qsuiv = new JButton(">");
+		qsuiv = new JButton(">");
 		qsuiv.setName("suiv");
-		qsuiv.addActionListener(new QuestionnaireButton(qsuiv.getName(), this.questionnaire));
-		
-		JButton qprec = new JButton("<");
+		qsuiv.setToolTipText("question suivante");
+		qsuiv.addActionListener(new QuestionnaireButton(qsuiv.getName(), this));//, this.q));
+			//question precedente
+		qprec = new JButton("<");
 		qprec.setName("prec");
-		qprec.addActionListener(new QuestionnaireButton(qprec.getName(), this.questionnaire));
+		qprec.setToolTipText("question precedente");
+		qprec.addActionListener(new QuestionnaireButton(qprec.getName(), this));//, this.q));
+		qprec.setEnabled(false);
 		
-		JButton qprem = new JButton("<<<");
+			//premiere question
+		qprem = new JButton("<<<");
 		qprem.setName("prem");
-		qprem.addActionListener(new QuestionnaireButton(qprem.getName(), this.questionnaire));
-		
-		JButton qder = new JButton(">>>");
+		qprem.setToolTipText("premiere question");
+		qprem.addActionListener(new QuestionnaireButton(qprem.getName(), this));//, this.q));
+		qprem.setEnabled(false);
+			// derniere question
+		qder = new JButton(">>>");
 		qder.setName("dern");
-		qder.addActionListener(new QuestionnaireButton(qder.getName(), this.questionnaire));
+		qder.setToolTipText("derniere question");
+		qder.addActionListener(new QuestionnaireButton(qder.getName(), this));//, this.q));
 		
+			// Terminer questionnaire
+		terminer = new JButton("Terminer");
+		terminer.setName("term");
+		terminer.setToolTipText("Terminer questionnaire");
+		terminer.addActionListener(new QuestionnaireButton(terminer.getName(), this));
+		terminer.setEnabled(false);
+		
+			
+		
+		// ajout des boutons au panel boutons
 		boutonsQuestions.add(qprem); 
 		boutonsQuestions.add(qprec);
+		boutonsQuestions.add(terminer);
 		boutonsQuestions.add(qsuiv); 
 		boutonsQuestions.add(qder); 
-
-		// creation de la vue de la question:
-			// par la suite on donnera une question et il creera la vue en fonction de la question
-		QuestionVue questionVue = new QuestionVueChoixOrdonnee();
 		
+		
+		System.out.println("QuestionVue creer");
 		questionVue.setBorder(BorderFactory.createLineBorder(Color.black));
-		questionVue.setPreferredSize(new Dimension(300,150));
+		//questionVue.setPreferredSize(new Dimension(300,150));
 		questionVue.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		System.out.println(">Jusqu'il ça passe");
+
+
+		
+		System.out.println("CREATION BOUTONS FIN");
+		// creation du panel contenant la vue de la question et le bouton terminer:
+		questionJ = new JPanel(new BorderLayout());
+		questionJ.add(questionVue, BorderLayout.CENTER);
+			
+		
+		
 		
 		//this.add(new JLabel("TESTTTTT"));
 		this.add(idQuestionnaireJ, BorderLayout.NORTH);
-		this.add(questionVue, BorderLayout.CENTER);
+		this.add(questionJ, BorderLayout.CENTER);
 		this.add(boutonsQuestions,BorderLayout.SOUTH); 		
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
+
+		//this.add(new JLabel("coucou"));
+		this.setPreferredSize(new Dimension(350,530));
+		this.revalidate();
 		
-		
+		System.out.println("CREA QUESTIONNAIRE FIN");
 	}
+	
+	public void ChangerQuestion(Question q){
+		this.questionJ.removeAll();
+		this.questionVue = null;
+		questionVue = new QuestionVue(q, this);
+		questionVue.setBorder(BorderFactory.createLineBorder(Color.black));
+		questionVue.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		questionJ.add(questionVue, BorderLayout.CENTER);
+		
+		//this.add(questionJ, BorderLayout.CENTER);
+		//this.questionVue.setVisible(true);
+		this.revalidate();
+		this.repaint();
+		//this.module.fenetre.repaint();
+		//this.module.fenetre.revalidate();
+	}
+	
+	public void TerminerQuestionnaireError(){
+		this.questionJ.remove(questionVue);		
+		qError = new QuestionnaireNonRempli(this);
+
+		this.questionJ.add(qError, BorderLayout.CENTER);
+		this.questionJ.setVisible(true);
+		this.revalidate();
+		this.repaint();
+	}
+	
 }
+
